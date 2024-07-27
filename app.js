8dd5701fb02aac93e470e0077122b27ce2b567ae
CastPJ <JPajak.dev@gmail.com>
2024-07-27 13:19:05 +0200

    Code modified to be used from within class (OOP)

--- app.js
+++ app.js
@@ -1,3 +1,74 @@
+class Note {
+  constructor(id) {
+    this.id = id;
+    this.sideNote = this.createSideNote();
+    this.mainNote = this.createMainNote();
+  }
+
+  createSideNote() {
+    const sideNote = document.createElement("div");
+    sideNote.classList.add("side-note");
+    sideNote.setAttribute("id", `side-${this.id}`);
+
+    const xMark = document.createElement("div");
+    xMark.classList.add("x-mark");
+    const xText = document.createElement("p");
+    xText.classList.add("x-mark-p");
+    xText.innerText = "x";
+
+    const sideNoteText = document.createElement("div");
+    sideNoteText.classList.add("center-content");
+
+    xMark.appendChild(xText);
+    sideNote.appendChild(xMark);
+    sideNote.appendChild(sideNoteText);
+
+    // Bind event listener for changing focus
+    sideNote.addEventListener("click", this.changeFocus.bind(this));
+
+    return sideNote;
+  }
+
+  createMainNote() {
+    const mainNote = document.createElement("div");
+    mainNote.classList.add("note");
+    mainNote.setAttribute("id", `main-${this.id}`);
+
+    const noteTitle = document.createElement("h1");
+    noteTitle.classList.add("title");
+    noteTitle.textContent = mainNote.id;
+
+    const noteText = document.createElement("p");
+    noteText.classList.add("content");
+
+    mainNote.appendChild(noteTitle);
+    mainNote.appendChild(noteText);
+
+    return mainNote;
+  }
+
+  addToDOM() {
+    sideGrid.appendChild(this.sideNote);
+    mainContent.appendChild(this.mainNote);
+  }
+
+  changeFocus(e) {
+    e.preventDefault();
+
+    const sideNoteId = e.currentTarget.id;
+
+    const mainNoteId = `main-${sideNoteId.split("-")[1]}`;
+    const allMainNotes = document.querySelectorAll(".note");
+    allMainNotes.forEach((note) => {
+      if (note.id === mainNoteId) {
+        note.style.display = "block";
+      } else {
+        note.style.display = "none";
+      }
+    });
+  }
+}
+
 const plusNote = document.getElementById("plus-note");
 const sideGrid = document.getElementById("side-grid");
 const mainContent = document.getElementById("main-content");
@@ -6,69 +77,17 @@ let id = 1;
 
 function addNote(e) {
   e.preventDefault();
+
   // Reset for focused note on adding a new one
   const allMainNotes = document.querySelectorAll(".note");
   allMainNotes.forEach((note) => {
     note.style.display = "none";
   });
 
-  // SIDE NOTE
-  const sideNote = document.createElement("div");
-  sideNote.classList.add("side-note");
-  sideNote.setAttribute("id", `side-${id}`);
-
-  // x-mark
-  const xMark = document.createElement("div");
-  xMark.classList.add("x-mark");
-  const xText = document.createElement("p");
-  xText.classList.add("x-mark-p");
-  xText.innerText = "x";
-
-  // note text / +
-  const sideNoteText = document.createElement("div");
-  sideNoteText.classList.add("center-content");
-
-  // MAIN NOTE
-  const mainNote = document.createElement("div");
-  mainNote.classList.add("note");
-  mainNote.setAttribute("id", `main-${id}`);
-  const noteTitle = document.createElement("h1");
-  noteTitle.classList.add("title");
-  noteTitle.textContent = mainNote.id;
-  const noteText = document.createElement("p");
-  noteText.classList.add("content");
-
-  // APPEND
-  //side
-  xMark.appendChild(xText);
-  sideNote.appendChild(xMark);
-  sideNote.appendChild(sideNoteText);
-  sideGrid.appendChild(sideNote);
-  //main
-  mainNote.appendChild(noteTitle);
-  mainNote.appendChild(noteText);
-  mainContent.appendChild(mainNote);
-
-  // Add event listener for the newly added side note
-  sideNote.addEventListener("click", changeFocus);
-
+  // Create and Add new note
+  const note = new Note(id);
+  note.addToDOM();
   id += 1;
-}
-
-function changeFocus(e) {
-  e.preventDefault();
-
-  const sideNoteId = e.currentTarget.id;
-
-  const mainNoteId = `main-${sideNoteId.split("-")[1]}`;
-  const allMainNotes = document.querySelectorAll(".note");
-  allMainNotes.forEach((note) => {
-    if (note.id === mainNoteId) {
-      note.style.display = "block";
-    } else {
-      note.style.display = "none";
-    }
-  });
 }
 
 plusNote.addEventListener("click", addNote);
